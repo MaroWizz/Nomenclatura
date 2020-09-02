f50ba3f25a95fab7d0b876449aa097e3c240c8c5
mcasteletti <mcasteletti@MCASTELETI-NTBK.azurian.cl>
2020-09-02 13:05:40 -0400

    Modifica el nombre de los archivos con separador de _ split("_")

--- Nomenclatura/src/Archivo.java
+++ Nomenclatura/src/Archivo.java
@@ -4,22 +4,19 @@ import java.io.IOException;
 public class Archivo{
 
     public static void main(String[] args) {
-    	
-    	if (args.length == 3) {
-    		String directorioCliente = args[0];
+
     		//Directorio del cliente, en al cual estan los archivos PDF en distintas carpetas
-    		File diradquirirArchivos = new File("C:\\Users\\mcasteletti\\Desktop\\76089459-1\\sent");
+    		File diradquirirArchivos = new File("C:\\nomenclatura\\Origen");
     		//Directorio en el cual se dejarán los documentos PDF
-    		File dirOrigen = new File ("C:\\Users\\mcasteletti\\Desktop\\76089459-1\\Origen\\");
+    		File dirOrigen = new File ("C:\\nomenclatura\\Transicion");
     		//Directorio de destino
-    		File dirDestino = new File ("C:\\Users\\mcasteletti\\Desktop\\76089459-1\\Destino\\");
+    		File dirDestino = new File ("C:\\nomenclatura\\Destino");
     		
     		adquirirArchivos(diradquirirArchivos, dirOrigen);
     		moverAFecha(dirOrigen,dirDestino);
     		cambiarNombre(dirDestino);
-    	} else {
-    		System.out.println("Ingrese argumentos");
-    	}
+
+    	
     }
 
 
@@ -55,11 +52,18 @@ public class Archivo{
     	File directorioDestino = dirDestino;
 
     	for (File file : files) {
-    		//System.out.println(file.getName());
+    		
     		String nombre = file.getName();
-    		String anho = nombre.substring(41,45);
-    		String mes = nombre.substring(45,47);
-    		String dia = nombre.substring(47,49);
+    		String[] parts = nombre.split("_");
+    		String date =parts[0];
+    		String[] dateArray = date.split("-");
+    		System.out.println("Fecha del documento" + date);
+    		String anho = dateArray[0];
+    		System.out.println("año: "+anho);
+    		String mes = dateArray[1];
+    		System.out.println("mes: "+mes);
+    		String dia = dateArray[2];
+    		System.out.println("dia: "+dia);
         	File fileDestinoAnho = new File(directorioDestino,anho);
         	File fileDestinoMes = new File(fileDestinoAnho,mes);
         	File fileDestinoDia = new File(fileDestinoMes,dia);
@@ -73,6 +77,7 @@ public class Archivo{
            	if (estadoMovimiento) System.out.println("Documento movido correctamente: "+ file.getName());
            	else System.out.println("Error al mover: " + file.getName());
            	}
+    	System.out.println("FIN");
     }
     
     
@@ -88,9 +93,17 @@ public class Archivo{
                 //Si no es un directorio, se comienzan a mover los documentos
                 } else {
                 	String nombre = file.getName();
-                	String rutEmisor = nombre.substring(21,29);
-            		int folio = Integer.parseInt(nombre.substring(6,20));
-            		int tipoDocu = Integer.parseInt(nombre.substring(1,4));
+                	String[] parts = nombre.split("_");
+                	
+                	String rutEmisorConExentecion = parts[4];
+
+
+                	String rutEmisor=rutEmisorConExentecion.substring(0,8);
+
+                	System.out.println("****"+rutEmisor);
+                	
+            		int folio = Integer.parseInt(parts[1]);
+            		int tipoDocu = Integer.parseInt(parts[2]);
             		File newNombre = new File (rutEmisor+"-"+folio+"-"+tipoDocu);
             		
             		System.out.println("Archivo a modificar: "+ file.getAbsolutePath());
@@ -99,14 +112,27 @@ public class Archivo{
             		System.out.println("Folio: "+folio);
             		System.out.println("TipoDocumento: "+tipoDocu);
             		
-            		File archivoAMover = new File(dirDestino +"\\"+ newNombre+".pdf");
-                  	boolean estadoMovimiento =  file.renameTo(archivoAMover);
-                 	if (estadoMovimiento) System.out.println("Documento modificado correctamente: "+ newNombre+".pdf");
-                    else System.out.println("Error al modificar nombre: " + file.getName());
-                 		
-                   	System.out.println("Ejecución Realizada");
-                   	System.out.println("==================");
+            		System.out.println(rutEmisorConExentecion.substring(9,12));
             		
+            		if (rutEmisorConExentecion.substring(9,12).equals("xml")) {
+            			File archivoAMover = new File(dirDestino +"\\"+ newNombre+".xml");
+                      	boolean estadoMovimiento =  file.renameTo(archivoAMover);
+                     	if (estadoMovimiento) System.out.println("Documento modificado correctamente: "+ newNombre+".xml");
+                        else System.out.println("Error al modificar nombre: " + file.getName());
+                     		
+                       	System.out.println("Ejecución Realizada");
+                       	System.out.println("==================");
+            		}
+            		if(rutEmisorConExentecion.substring(9,12).equals("pdf")) {
+            			File archivoAMover = new File(dirDestino +"\\"+ newNombre+".pdf");
+                      	boolean estadoMovimiento =  file.renameTo(archivoAMover);
+                     	if (estadoMovimiento) System.out.println("Documento modificado correctamente: "+ newNombre+".pdf");
+                        else System.out.println("Error al modificar nombre: " + file.getName());
+                     		
+                       	System.out.println("Ejecución Realizada");
+                       	System.out.println("==================");
+            		}
+
                 }
             }
         } catch (IOException e) {
